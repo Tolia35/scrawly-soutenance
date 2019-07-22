f532b431b1e192a84d7adedd92228c46349054aa
Tolia35 <Anadim35>
2019-07-22 14:48:19 +0200

    Ajout update slug

---
+++ scrawly-react/src/components/Homepage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Homepage(props){
+    return(
+        <form>
+            <input type="text" value={props.slug} onChange={event =>props.updateSlug(event.target.value)}></>
+        </form>
+    );
+}
+export default Homepage;
--- scrawly-react/src/index.js
+++ scrawly-react/src/index.js
@@ -1,5 +1,3 @@
-// importer nos dépendances
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -7,12 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {createStore} from "redux";
 import {Provider} from "react-redux";
-import scrawlyApplication from "./reducers/reducers";
 import {BrowserRouter} from 'react-router-dom';
+import scrawlyApplication from "./reducers/reducers";
 
 const store = createStore(scrawlyApplication);
 
-ReactDOM.render(<provider store = {store}><BrowserRouter><App /></BrowserRouter></provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store = {store}><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
